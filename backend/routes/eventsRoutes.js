a3e050547d2b3ab7e97cb3a5b9e0d7d2c128de1f
RajeshKumawat0101 <rajeshkumawat01012003@gmail.com>
2024-10-23 23:03:16 +0530

    removed

--- backend/routes/eventsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventControllers = require('../controllers/eventsControllers');
-
-
-router.get('/weekly-leaderboard',eventControllers.weeklyLeaderBoard); // returns weekly leaderboard
-
-module.exports = router;
-
